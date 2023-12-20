0529b7f5f7ae54912a6c6679af2c24ed4132d0d1
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-20 14:56:13 +0000

    feat: add home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.sass'
+})
+export class HomeComponent {
+
+}
