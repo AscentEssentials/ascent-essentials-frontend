bc650fc2a2b9c7af3309d6ed308d590bb0204ab2
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-20 15:06:19 +0000

    feat: add order-history component

---
+++ src/app/order-history/order-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-history.component.html',
+  styleUrl: './order-history.component.sass'
+})
+export class OrderHistoryComponent {
+
+}
