c528e8e8426197d6f008e6b06c0aa6ab9e08ae25
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-20 16:20:23 +0000

    chore: add example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CurrencyPipe, NgForOf, NgOptimizedImage} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    NgForOf,
+    MatButtonModule,
+    CurrencyPipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: any[] = []; // Replace with your actual product data
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Fetch or set your product data here
+    this.products = [
+      {
+        name: 'Climbing Shoes',
+        description: 'High-performance climbing shoes for all terrains.',
+        price: 99.99
+      },
+      // Add more products as needed
+    ];
+  }
 }
