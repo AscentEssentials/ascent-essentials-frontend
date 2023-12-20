f060e02461c680224e5fd21d153ea6fe996407d4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-20 15:29:50 +0000

    chore: change page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'ascent-essentials';
+  title = 'Ascent Essentials';
 }
