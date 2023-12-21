67e0bdaef308757374f29db0cb8d06da965d8d2b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-21 16:23:44 +0000

    feat: improve header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,8 @@ import {RouterLink} from "@angular/router";
   imports: [
     MatToolbarModule,
     MatButtonModule,
-    RouterLink
+    RouterLink,
+    MatIconModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
