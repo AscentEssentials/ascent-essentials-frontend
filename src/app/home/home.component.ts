a96777e3d2ea6d35bcd18bf053d12bd149ee70e8
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-21 16:24:10 +0000

    feat: add tiles in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {CurrencyPipe, NgForOf, NgOptimizedImage} from "@angular/common";
-import {MatButtonModule} from "@angular/material/button";
+import {Component} from '@angular/core';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {Tile} from "./Tile";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    NgOptimizedImage,
-    NgForOf,
-    MatButtonModule,
-    CurrencyPipe
-  ],
   templateUrl: './home.component.html',
+  imports: [
+    MatGridListModule,
+    NgForOf,
+    NgOptimizedImage
+  ],
   styleUrl: './home.component.sass'
 })
-export class HomeComponent implements OnInit {
-  products: any[] = []; // Replace with your actual product data
-
-  constructor() { }
-
-  ngOnInit(): void {
-    // Fetch or set your product data here
-    this.products = [
-      {
-        name: 'Climbing Shoes',
-        description: 'High-performance climbing shoes for all terrains.',
-        price: 99.99
-      },
-      // Add more products as needed
-    ];
-  }
+export class HomeComponent {
+  tiles: Tile[] = [
+    {cols: 2, rows: 6, imagePath: './assets/images/cordeo-hpd.jpg'},
+    {cols: 1, rows: 10, imagePath: './assets/images/abbigliamento-hpm.jpg'},
+    {cols: 1, rows: 10, imagePath: './assets/images/3scarpette_generatormid.jpg'},
+    {cols: 2, rows: 6, imagePath: './assets/images/imbrago-hpd.jpg'},
+  ];
 }
