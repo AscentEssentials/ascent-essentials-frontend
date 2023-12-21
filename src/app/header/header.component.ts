2e1a92746f88942102337ba8bcea911d591ceef4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-21 10:03:09 +0000

    refactor: remove unused components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLink
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
