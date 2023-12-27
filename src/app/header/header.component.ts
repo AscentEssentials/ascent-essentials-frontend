36a417f1984a157614442cb5791ac6654113dc7c
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-27 15:30:07 +0000

    feat: add search bar in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,9 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {RouterLink} from "@angular/router";
 import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-header',
@@ -11,7 +14,10 @@ import {MatIconModule} from "@angular/material/icon";
     MatToolbarModule,
     MatButtonModule,
     RouterLink,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
