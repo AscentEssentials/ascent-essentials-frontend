e91247e71aefeb740b43d84e35523a676b586816
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-28 21:36:22 +0000

    refactor: improve footer style and remove section

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {MatListModule} from "@angular/material/list";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MatListModule
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.sass'
