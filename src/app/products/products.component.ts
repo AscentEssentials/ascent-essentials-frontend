8dc63e2f330a6f4193d5e5d92eec9d43dcec2f2b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-30 14:25:25 +0000

    feat: add product card in products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {Product} from "../product";
+import {ProductCardComponent} from "../product-card/product-card.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductCardComponent,
+    NgForOf
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.sass'
 })
 export class ProductsComponent {
-
+  products: Product[] = [
+    {
+      name: 'Product 1',
+      imagePath: '../../assets/images/placeholder.jpg',
+      price: 29.99,
+    },
+    {
+      name: 'Product 2',
+      imagePath: '../../assets/images/placeholder.jpg',
+      price: 39.99,
+    },
+  ];
 }
