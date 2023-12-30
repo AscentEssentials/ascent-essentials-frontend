b11ebd9913b21db435fe728036cd509ef866429f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-30 15:38:10 +0000

    feat: add paginator and price filter in products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import {Product} from "../product";
 import {ProductCardComponent} from "../product-card/product-card.component";
 import {NgForOf} from "@angular/common";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatInputModule} from "@angular/material/input";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-products',
   standalone: true,
   imports: [
     ProductCardComponent,
-    NgForOf
+    NgForOf,
+    MatPaginatorModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.sass'
@@ -65,5 +73,24 @@ export class ProductsComponent {
       imagePath: '../../assets/images/placeholder.jpg',
       price: 29.99,
     },
-  ];
+  ]
+
+  filteredProducts: Product[]
+  priceFilterForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.priceFilterForm = this.formBuilder.group({
+      minPrice: [0],
+      maxPrice: [100],
+    });
+    this.filteredProducts = [...this.products]
+  }
+
+  applyFilter() {
+    const minPrice = this.priceFilterForm.value.minPrice;
+    const maxPrice = this.priceFilterForm.value.maxPrice;
+    this.filteredProducts = this.products.filter(
+      (product) => product.price >= minPrice && product.price <= maxPrice
+    );
+  }
 }
