004c1006eb07c54bc52338c768ca50de64379b9d
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-31 15:27:04 +0000

    feat: add checkout page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.sass'
 })
 export class CheckoutComponent {
-
+  executeOrder() {
+    console.log('Order Executed!');
+  }
 }
