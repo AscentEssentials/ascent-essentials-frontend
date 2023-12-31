0d1a5cb81e919af474405e491b765ce82ae67a70
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-31 15:44:04 +0000

    feat: improve checkout page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
+import {MatStepperModule} from "@angular/material/stepper";
 
 @Component({
   selector: 'app-checkout',
@@ -11,7 +12,8 @@ import {MatButtonModule} from "@angular/material/button";
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatStepperModule
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.sass'
