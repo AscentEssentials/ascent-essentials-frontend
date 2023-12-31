18416b64cdac51024d23a7d4c99debcf37eb41af
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-31 14:45:36 +0000

    feat: implement customer-login.component

--- src/app/customer-login/customer-login.component.ts
+++ src/app/customer-login/customer-login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-customer-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './customer-login.component.html',
   styleUrl: './customer-login.component.sass'
 })
