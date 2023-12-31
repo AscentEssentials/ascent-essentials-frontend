9afd80adc3c673fcba7367b96b8f49c719643f0a
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-12-31 11:34:06 +0000

    refactor: use Product in product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,6 +3,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {CurrencyPipe, NgClass, NgForOf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {CartProduct} from "../cart-product";
 
 @Component({
   selector: 'app-product',
@@ -18,14 +19,20 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrl: './product.component.sass'
 })
 export class ProductComponent {
-  productName: string = "Kataki";
-  productBrand: string = "La Sportiva";
-  productImages: string[] = ["../../assets/images/placeholder.jpg", "../../assets/images/placeholder.jpg"];
-  productDescription: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-  productPrice: number = 29.90;
-  isAvailable: boolean = true;
-  selectedQuantity: number = 1;
-  currentImage: string = "../../assets/images/placeholder.jpg";
+
+  product: CartProduct = {
+    product: {
+      name: "Kataki",
+      brand: "La Sportiva",
+      imagesPaths: ["../../assets/images/placeholder.jpg", "../../assets/images/placeholder.jpg"],
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      price: 29.90,
+      isAvailable: true,
+    },
+    quantity: 1,
+  }
+
+  currentImage: string = "../../assets/images/placeholder.jpg"
 
   constructor(private snackBar: MatSnackBar) {}
 
@@ -35,7 +42,7 @@ export class ProductComponent {
 
   addToCart() {
     // Implement your logic for adding to the cart here
-    const message = `${this.selectedQuantity} ${this.productBrand} ${this.productName}(s) added to the cart.`;
+    const message = `${this.product.quantity} ${this.product.product.brand} ${this.product.product.name}(s) added to the cart.`;
 
     this.snackBar.open(message, 'Close', {
       duration: 5000,
