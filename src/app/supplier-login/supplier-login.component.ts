1b45677a96fcf1d3f38b4359f47db9e9f1c77fb3
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-02 15:55:58 +0000

    feat: add supplier-login component

--- src/app/supplier-login/supplier-login.component.ts
+++ src/app/supplier-login/supplier-login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-supplier-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './supplier-login.component.html',
   styleUrl: './supplier-login.component.sass'
 })
 export class SupplierLoginComponent {
-
+  login() {
+    console.log('Supplier login clicked!')
+  }
 }
