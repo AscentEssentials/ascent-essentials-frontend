8864775e7d64721417fb53f1b3ddc40fe91c432b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-03 18:17:31 +0000

    feat: add url in services

--- src/app/subcategory-service.service.ts
+++ src/app/subcategory-service.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubcategoryServiceService {
+  url = environment.apiUrl
 
   constructor() { }
 }
