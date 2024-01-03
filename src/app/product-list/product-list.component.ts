41881c2b07ca71dccec49e8bb1b03704086665c4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-03 10:37:36 +0000

    feat: add product-list and product-management components

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,145 @@
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {Product} from "../product";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {FormsModule} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {ProductManagementComponent} from "../product-management/product-management.component";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    FormsModule,
+    MatIconModule,
+    RouterLink
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.sass'
+})
+export class ProductListComponent implements AfterViewInit {
+  displayedColumns: string[] = ['name', 'brand', 'actions'];
+  products: Product[] = [
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+    {
+      name: 'Product',
+      imagesPaths: ['../../assets/images/placeholder.jpg'],
+      price: 29.99,
+      brand: 'brand',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      isAvailable: true,
+    },
+  ]
+
+  searchTerm: string = '';
+  filteredProducts: MatTableDataSource<Product> = new MatTableDataSource(this.products);
+
+  constructor(public dialog: MatDialog, private snackBar: MatSnackBar) {}
+
+  deleteProduct(product: Product): void {
+    const index = this.products.indexOf(product);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      this.filteredProducts.data = [...this.products];
+      this.snackBar.open('Product deleted successfully', 'OK', { duration: 2000 });
+    } else {
+      this.snackBar.open('Error deleting product', 'OK', { duration: 2000 });
+    }
+  }
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.filteredProducts.paginator = this.paginator;
+  }
+
+  search(): void {
+    this.filteredProducts.data = this.products.filter(
+      (product) =>
+        product.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        product.brand.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+}
