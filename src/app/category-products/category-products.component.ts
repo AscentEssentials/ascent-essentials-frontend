5d2e747f33064edaec43e1b7a9bdf7577c42bfae
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-05 17:17:54 +0000

    feat: add category-products and subcategory-products

---
+++ src/app/category-products/category-products.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductsComponent} from "../products/products.component";
+import {ProductService} from "../product.service";
+import {Product} from "../product";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-category-products',
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    ProductsComponent,
+  ],
+  providers: [ProductService],
+  templateUrl: './category-products.component.html',
+  styleUrl: './category-products.component.sass'
+})
+export class CategoryProductsComponent implements OnInit {
+  categoryId: string = ""
+  products: Product[] = []
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.categoryId = params['categoryId']
+      this.productService.getProductsByCategory(this.categoryId).subscribe(products => {
+        this.products = products
+      })
+    });
+  }
+}
---
+++ src/app/subcategory-products/subcategory-products.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {ProductService} from "../product.service";
+import {Product} from "../product";
+import {ProductsComponent} from "../products/products.component";
+
+@Component({
+  selector: 'app-subcategory-products',
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    ProductsComponent,
+  ],
+  providers: [ProductService],
+  templateUrl: './subcategory-products.component.html',
+  styleUrl: './subcategory-products.component.sass'
+})
+export class SubcategoryProductsComponent implements OnInit {
+  subcategoryId: string = ""
+  products: Product[] = []
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.subcategoryId = params['subcategoryId']
+      this.productService.getProductsBySubcategory(this.subcategoryId).subscribe(products => {
+        this.products = products
+      })
+    });
+  }
+}
