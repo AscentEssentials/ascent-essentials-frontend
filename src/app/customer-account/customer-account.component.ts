86dabae5bc470672a34039216a31dc671cbc4d2b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-06 17:18:50 +0000

    feat: now it is possible to logout

--- src/app/customer-account/customer-account.component.ts
+++ src/app/customer-account/customer-account.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CurrencyPipe, DatePipe} from "@angular/common";
 import {MatTableModule} from "@angular/material/table";
 import {MatPaginatorModule} from "@angular/material/paginator";
+import {UserService} from "../user.service";
+import {CookieService} from "ngx-cookie-service";
+import {HttpClientModule} from "@angular/common/http";
+import {User} from "../user";
+import {MatButtonModule} from "@angular/material/button";
+import {Router} from "@angular/router";
+import {routes} from "../app.routes";
 
 @Component({
   selector: 'app-customer-account',
@@ -10,19 +17,17 @@ import {MatPaginatorModule} from "@angular/material/paginator";
     CurrencyPipe,
     DatePipe,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    HttpClientModule,
+    MatButtonModule,
   ],
+  providers: [UserService],
   templateUrl: './customer-account.component.html',
   styleUrl: './customer-account.component.sass'
 })
-export class CustomerAccountComponent {
-  name: string = 'John';
-  surname: string = 'Doe';
-  email: string = 'john.doe@example.com';
-  address: string = '123 Main Street';
-  number: string = 'Apt 4B';
-  zip: string = '12345';
-  phoneNumber: string = '+1 (555) 123-4567';
+export class CustomerAccountComponent implements OnInit {
+  customer?: User
+
   orders = [
     { orderNumber: '12345', date: new Date(), totalPrice: 39.98 },
     { orderNumber: '54321', date: new Date(), totalPrice: 59.99 },
@@ -41,4 +46,18 @@ export class CustomerAccountComponent {
   ];
   pageSize = 5;
   pageSizeOptions: number[] = [5, 10, 25, 50];
+
+  constructor(private router: Router, private userService: UserService, private cookieService: CookieService) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getUserDetails(this.cookieService.get("accessToken")).subscribe(user => {
+      this.customer = user
+    })
+  }
+
+  logout() {
+    this.cookieService.delete("accessToken")
+    this.router.navigate(["/home"])
+  }
 }
