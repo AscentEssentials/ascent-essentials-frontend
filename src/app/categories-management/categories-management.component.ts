a88dd0f4713297ed4a178bc0e291930b3acb2809
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-07 18:09:15 +0000

    feat: now it is possible to add new subcategory

--- src/app/categories-management/categories-management.component.ts
+++ src/app/categories-management/categories-management.component.ts
@@ -6,9 +6,10 @@ import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSelectModule} from "@angular/material/select";
-import {Subcategory} from "../subcategory";
+import {SubcategoryResponse} from "../subcategory";
 import {CategoryService} from "../category.service";
 import {PermissionService} from "../permission.service";
+import {Category} from "../category";
 
 @Component({
   selector: 'app-categories-management',
@@ -29,40 +30,66 @@ import {PermissionService} from "../permission.service";
   styleUrl: './categories-management.component.sass'
 })
 export class CategoriesManagementComponent implements OnInit {
-  categoryForm: FormGroup;
-  existingSubcategories: Subcategory[] = []
+  editCategoryForm: FormGroup;
+  newCategoryForm: FormGroup;
+  existingSubcategories: SubcategoryResponse[] = []
+  existingCategories: Category[] = [];
 
   constructor(private fb: FormBuilder, private categoryService: CategoryService, private permissionService: PermissionService) {
-    this.categoryForm = this.fb.group({
+    this.editCategoryForm = this.fb.group({
       categoryId: ['', Validators.required],
       newName: ['', Validators.required],
       newDescription: ['', Validators.required],
     })
-  }
-
-  ngOnInit(): void {
-    this.getSubcategories()
-  }
-
-  getSubcategories() {
-    this.categoryService.getAllSubcategories().subscribe(subcategories => {
-      this.existingSubcategories = subcategories
+    this.newCategoryForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      parentCategoryId: ['', Validators.required],
     })
   }
 
-  submitForm() {
-    const newName: string = this.categoryForm.value.newName
-    const newDescription: string = this.categoryForm.value.newDescription
-    const selectedSubcategoryId: string = this.categoryForm.value.categoryId
+  ngOnInit(): void {
+    this.loadCategories()
+  }
+
+  loadCategories() {
+    this.categoryService.getAllSubcategories().subscribe(subcategories => {
+      this.existingSubcategories = subcategories
+    })
+    this.categoryService.getAllCategories().subscribe(categories => {
+      this.existingCategories = categories
+    })
+  }
+
+  submitEditCategoryForm() {
+    const newName: string = this.editCategoryForm.value.newName
+    const newDescription: string = this.editCategoryForm.value.newDescription
+    const selectedSubcategoryId: string = this.editCategoryForm.value.categoryId
     const selectedSubcategory = this.existingSubcategories.find(subcategory => subcategory._id == selectedSubcategoryId)!!
-    const newSubcategory: Subcategory = {
+    const newSubcategory: SubcategoryResponse = {
       _id: selectedSubcategory._id,
       categoryId: selectedSubcategory?.categoryId,
       name: newName,
       description: newDescription
     }
     this.categoryService.editSubcategory(newSubcategory, this.permissionService.getToken().token).subscribe(_ => {
-      this.getSubcategories()
+      this.loadCategories()
+    })
+  }
+
+  submitNewCategoryForm() {
+    const name: string = this.newCategoryForm.value.name
+    const description: string = this.newCategoryForm.value.description
+    const parentCategoryId: string = this.newCategoryForm.value.parentCategoryId
+    this.categoryService.createSubcategory(
+      {
+        name: name,
+        description: description,
+        categoryId: parentCategoryId,
+      },
+      this.permissionService.getToken().token
+    ).subscribe(_ => {
+      this.loadCategories()
     })
   }
 }
