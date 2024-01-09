3f91f385f2a32832d095c2ad7b74f82b124c13ab
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-09 10:50:10 +0000

    feat: now it is possible to apply filters

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {ProductResponse} from "../product";
 import {ProductCardComponent} from "../product-card/product-card.component";
 import {NgForOf} from "@angular/common";
@@ -6,9 +6,13 @@ import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatInputModule} from "@angular/material/input";
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
-import {ProductService} from "../product.service";
 import {HttpClientModule} from "@angular/common/http";
 import {RouterLink} from "@angular/router";
+import {MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {SubcategoryResponse} from "../subcategory";
+import {CategoryService} from "../category.service";
+import {ProductService} from "../product.service";
 
 @Component({
   selector: 'app-products',
@@ -21,31 +25,54 @@ import {RouterLink} from "@angular/router";
     ReactiveFormsModule,
     MatButtonModule,
     HttpClientModule,
-    RouterLink
+    RouterLink,
+    MatOptionModule,
+    MatSelectModule
   ],
-  providers: [ProductService],
+  providers: [CategoryService, ProductService],
   templateUrl: './products.component.html',
   styleUrl: './products.component.sass'
 })
-export class ProductsComponent{
+export class ProductsComponent implements OnInit, OnChanges {
   @Input() products: ProductResponse[] = []
-
   filteredProducts: ProductResponse[]
-  priceFilterForm: FormGroup;
+  brands: string[] = []
+  subcategories: SubcategoryResponse[] = []
+  filterForm: FormGroup
 
-  constructor(private formBuilder: FormBuilder) {
-    this.priceFilterForm = this.formBuilder.group({
+  constructor(private formBuilder: FormBuilder, private categoryService: CategoryService) {
+    this.filterForm = this.formBuilder.group({
       minPrice: [0],
-      maxPrice: [100],
+      maxPrice: [5000],
+      subCategoryId: "",
+      brand: "",
     });
+    this.brands = this.products.map(p => p.brand)
     this.filteredProducts = [...this.products]
   }
 
-  applyFilter() {
-    const minPrice = this.priceFilterForm.value.minPrice;
-    const maxPrice = this.priceFilterForm.value.maxPrice;
+  ngOnInit(): void {
+    this.categoryService.getAllSubcategories().subscribe(subcategories =>{
+      this.subcategories = subcategories
+    })
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.filteredProducts = [...changes['products'].currentValue]
+    this.brands = [...changes['products'].currentValue].map(p => p.brand)
+  }
+
+  applyFilters() {
+    this.filteredProducts = [...this.products]
+    const minPrice = this.filterForm.value.minPrice
+    const maxPrice = this.filterForm.value.maxPrice
+    const subcategoryId = this.filterForm.value.subCategoryId
     this.filteredProducts = this.products.filter(
-      (product) => product.price >= minPrice && product.price <= maxPrice
+      (product) => {
+        return product.price >= minPrice
+          && product.price <= maxPrice
+          && product.subCategoryId == subcategoryId
+      }
     )
   }
 }
