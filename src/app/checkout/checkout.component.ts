76e6abe6a22c807661178d520875586282b090cb
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-09 17:31:25 +0000

    feat: now it is possible to execute an order

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -4,6 +4,11 @@ import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
 import {MatStepperModule} from "@angular/material/stepper";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {OrderService} from "../order.service";
+import {PermissionService} from "../permission.service";
+import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-checkout',
@@ -13,13 +18,41 @@ import {MatStepperModule} from "@angular/material/stepper";
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
-    MatStepperModule
+    MatStepperModule,
+    ReactiveFormsModule
   ],
+  providers: [OrderService, PermissionService],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.sass'
 })
 export class CheckoutComponent {
+  creditCardForm: FormGroup;
+  couponForm: FormGroup;
+
+  constructor(private snackBar: MatSnackBar, private router: Router, private fb: FormBuilder, private orderService: OrderService, private permissionService: PermissionService) {
+    this.creditCardForm = this.fb.group({
+      creditCardNumber: ['', [Validators.required]],
+      creditCardExpirationDate: ['', [Validators.required]],
+      ccv: ['', [Validators.required]],
+    });
+    this.couponForm = this.fb.group({
+      couponCode: ['']
+    });
+  }
   executeOrder() {
-    console.log('Order Executed!');
+    const formData = new FormData()
+    formData.append("creditCardNumber", this.creditCardForm.value.creditCardNumber)
+    formData.append("creditCardExpirationDate", this.creditCardForm.value.creditCardExpirationDate)
+    formData.append("ccv", this.creditCardForm.value.CCV)
+    formData.append("coupon", this.couponForm.value.coupon)
+    this.orderService.createOrder(formData, this.permissionService.getToken()).subscribe(_ => {
+      this.router.navigate(["/user/cart"]).then(_ => {
+        this.snackBar.open("Order executed!", 'Close', {
+          duration: 5000,
+          verticalPosition: 'bottom',
+          panelClass: ['custom-snackbar']
+        })
+      })
+    })
   }
 }
--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../environments/environment";
+import {NewOrder} from "./order";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Token} from "./user";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +11,10 @@ import {environment} from "../environments/environment";
 export class OrderService {
   apiUrl = environment.apiUrl
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createOrder(order: FormData, token: Token): Observable<Object>{
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token.token}`)
+    return this.http.post(`${this.apiUrl}/order`, order, {headers});
+  }
 }
