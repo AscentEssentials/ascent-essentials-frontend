bec9aedcdd899bf3aeeee39e3baf747a3847e1e4
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-01-09 17:46:00 +0000

    feat: now it is possible to see the executed orders for the customer

--- src/app/order.ts
+++ src/app/order.ts
@@ -1,6 +1,28 @@
-export interface NewOrder {
-  creditCardNumber: number,
-  creditCardExpirationDate: Date,
-  CCV: number,
-  coupon: string,
+export interface OrderResponse {
+  _id: string;
+  userId: string;
+  userAddress: {
+    address: string;
+    addressNumber: string;
+    zipCode: string;
+    telephoneNumber: string;
+  };
+  items: {
+    productId: string;
+    name: string;
+    brand: string;
+    price: number;
+    subCategoryId: string;
+    description: string;
+    technicalSpecifications: {
+      type: string;
+      diameter: string;
+    };
+    quantity: number;
+    images: string[];
+  }[]
+  shippingCosts: number;
+  orderTotal: number;
+  status: string;
+  createdAt: string;
 }
